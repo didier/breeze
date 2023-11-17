78e9531bdcaadf15a45f049c4a80d5c0dc82103e
Didier Catz <didier@catz.me>
2023-11-17 19:20:50 +0100

    Add base page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="h-screen w-screen grid place-items-center text-center">
+	<h1 class="text-7xl font-medium">Hello universe.</h1>
+</div>
